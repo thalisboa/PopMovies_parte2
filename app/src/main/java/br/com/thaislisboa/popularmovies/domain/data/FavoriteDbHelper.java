38160cd6daa7ea8cd15f504623659739119979af
Cassio Lima <cassio.menabue@gmail.com>
2017-12-28 10:38:50 -0200

    Ajuste no nome da classe FavoriteDbHelper

--- app/src/main/java/br/com/thaislisboa/popularmovies/domain/data/FavoriteDbHelper.java
+++ app/src/main/java/br/com/thaislisboa/popularmovies/domain/data/FavoriteDbHelper.java
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class TaskDbHelper extends SQLiteOpenHelper{
+public class FavoriteDbHelper extends SQLiteOpenHelper{
 
 
-    public TaskDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public FavoriteDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
