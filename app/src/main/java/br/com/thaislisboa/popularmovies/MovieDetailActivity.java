3d9312ab65e8401ad8f915b8d80ab8bd8448143a
Cassio Lima <cassio.menabue@gmail.com>
2017-12-28 17:55:46 -0200

    Comment Class MovieDetailActivity

--- app/src/main/java/br/com/thaislisboa/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/br/com/thaislisboa/popularmovies/MovieDetailActivity.java
@@ -30,7 +30,7 @@ import java.util.Scanner;
 import br.com.thaislisboa.popularmovies.domain.model.Movie;
 
 public class MovieDetailActivity extends AppCompatActivity {
-
+/*
     private Movie movie;
     private RecyclerView mRecyclerViewTrailer, mRecyclerViewReview;
     private String appKey;
@@ -79,8 +79,8 @@ public class MovieDetailActivity extends AppCompatActivity {
 
         }
 
-        new TrailerAsyncTask().execute(movie);
-        new ReviewAsyncTask().execute(movie);
+        // new TrailerAsyncTask().execute(movie);
+        // new ReviewAsyncTask().execute(movie);
 
 
         //Verificar
@@ -91,141 +91,42 @@ public class MovieDetailActivity extends AppCompatActivity {
 
                 //Quando clicar aqui a estrela habilitar , vou pegar as informacoes principais dos movies e passar para o contect resolver
 
-               // if (!mStar.setClickable(true)) {
+                // if (!mStar.setClickable(true)) {
 
-                    Toast.makeText(Context.this, "You are removing your movie in your Favorite List", Toast.LENGTH_LONG)
+                Toast.makeText(getBaseContext(), "You are removing your movie in your Favorite List", Toast.LENGTH_LONG)
 
 
-                } if(mStar.OnClickListener(false)){
+            } if(
+                    mStar.setOnClickListener(false)
+                    )
 
-                Toast.makeText(Context.this, "You are adding your movie in your Favorite List", Toast.LENGTH_LONG);
+            {
+
+                Toast.makeText(getBaseContext(), "You are adding your movie in your Favorite List", Toast.LENGTH_LONG);
 
 
             }
 
-    class TrailerAsyncTask extends AsyncTask<Movie, Movie, Movie> {
+            class TrailerAsyncTask extends AsyncTask<Movie, Movie, Movie> {
 
-        @Override
-        protected void onPostExecute(Movie movie) {
-            MovieDetailActivity.this.movie = movie;
-            mRecyclerViewTrailer.setAdapter(new TrailerAdapter(movie));
-            super.onPostExecute(movie);
-        }
-
-        @Override
-        protected Movie doInBackground(Movie... movies) {
-
-            try {
-                URL url = new URL(Uri.parse("http://api.themoviedb.org/3/movie/" + movies[0].getId() + "/videos")
-                        .buildUpon()
-                        .appendQueryParameter("api_key", appKey)
-                        .build().toString());
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-
-                StringBuilder content = new StringBuilder();
-                try (InputStream in = urlConnection.getInputStream();
-                     Scanner data = new Scanner(in)) {
-                    data.useDelimiter("\n");
-                    while (data.hasNext()) {
-                        content.append(data.next());
-                    }
-
-                    JSONObject json = new JSONObject(content.toString());
-                    JSONArray results = json.getJSONArray("results");
-
-
-                    for (int i = 0; i < results.length(); i++) {
-                        json = results.getJSONObject(i);
-
-                        movie.addTrailer(json.getString("name"),
-                                json.getString("key"));
-                    }
-                } finally {
-                    urlConnection.disconnect();
+                @Override
+                protected void onPostExecute(Movie movie) {
+                    MovieDetailActivity.this.movie = movie;
+                    mRecyclerViewTrailer.setAdapter(new TrailerAdapter(movie));
+                    super.onPostExecute(movie);
                 }
-            } catch (Exception cause) {
-                Log.e("MV", cause.getMessage(), cause);
-            }
 
-            return movie;
-        }
-    }
-
-    class TrailerViewHolder extends RecyclerView.ViewHolder {
-        TrailerViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
-
-    class TrailerAdapter extends RecyclerView.Adapter {
-
-        private Movie movie;
-
-
-        TrailerAdapter(Movie movie) {
-            this.movie = movie;
-        }
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(MovieDetailActivity.this)
-                    .inflate(R.layout.trailer_item_view, parent, false);
-            return new TrailerViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            TextView mTextView = holder.itemView.findViewById(R.id.tv_title);
-            holder.itemView.setOnClickListener((View e) -> {
-
-
-                Intent webIntent = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse(movie.getTrailerYoutube(position)));
-
-                startActivity(webIntent);
-
-            });
-
-
-            mTextView.setText(movie.getTrailerName(position));
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return movie.getTrailersSize();
-        }
-    }
-        //REVIEW// B.O TA AQUI
-
-        class ReviewAsyncTask extends AsyncTask<Movie, Movie, Movie> {
-
-            @Override
-            protected void onPostExecute(Movie movie) {
-                MovieDetailActivity.this.movie = movie;
-                mRecyclerViewReview.setAdapter(new ReviewAdapter(movie));
-                super.onPostExecute(movie);
-            }
-
-            @Override
-            protected Movie doInBackground(Movie... movies) {
-                HttpURLConnection urlConnection;
-
-                movie = movies[0];
-
-                try {
-                    URL url;
-                    String urls = "http://api.themoviedb.org/3/movie/" + movie.getId() + "/reviews";
-
-                    url = new URL(Uri.parse(urls)
-                            .buildUpon()
-                            .appendQueryParameter("api_key", appKey)
-                            .build().toString());
-
-                    urlConnection = (HttpURLConnection) url.openConnection();
+                @Override
+                protected Movie doInBackground(Movie... movies) {
 
                     try {
+                        URL url = new URL(Uri.parse("http://api.themoviedb.org/3/movie/" + movies[0].getId() + "/videos")
+                                .buildUpon()
+                                .appendQueryParameter("api_key", appKey)
+                                .build().toString());
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+
                         StringBuilder content = new StringBuilder();
                         try (InputStream in = urlConnection.getInputStream();
                              Scanner data = new Scanner(in)) {
@@ -241,57 +142,163 @@ public class MovieDetailActivity extends AppCompatActivity {
                             for (int i = 0; i < results.length(); i++) {
                                 json = results.getJSONObject(i);
 
-                                movie.addReview(json.getString("author"),
-                                        json.getString("content"));
+                                movie.addTrailer(json.getString("name"),
+                                        json.getString("key"));
                             }
+                        } finally {
+                            urlConnection.disconnect();
                         }
-                    } finally {
-                        urlConnection.disconnect();
+                    } catch (Exception cause) {
+                        Log.e("MV", cause.getMessage(), cause);
                     }
-                } catch (Exception cause) {
-                    Log.e("MV", cause.getMessage(), cause);
-                }
 
-                return movie;
+                    return movie;
+                }
             }
 
-        }
+            class TrailerViewHolder extends RecyclerView.ViewHolder {
+                TrailerViewHolder(View itemView) {
+                    super(itemView);
+                }
+            }
 
-    class ReviewViewHolder extends RecyclerView.ViewHolder {
-        ReviewViewHolder(View itemView) {
-            super(itemView);
+            class TrailerAdapter extends RecyclerView.Adapter {
+
+                private Movie movie;
+
+
+                TrailerAdapter(Movie movie) {
+                    this.movie = movie;
+                }
+
+                @Override
+                public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                    View view = LayoutInflater.from(MovieDetailActivity.this)
+                            .inflate(R.layout.trailer_item_view, parent, false);
+                    return new TrailerViewHolder(view);
+                }
+
+                @Override
+                public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                    TextView mTextView = holder.itemView.findViewById(R.id.tv_title);
+                    holder.itemView.setOnClickListener((View e) -> {
+
+
+                        Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse(movie.getTrailerYoutube(position)));
+
+                        startActivity(webIntent);
+
+                    });
+
+
+                    mTextView.setText(movie.getTrailerName(position));
+
+                }
+
+                @Override
+                public int getItemCount() {
+                    return movie.getTrailersSize();
+                }
+            }
+            //REVIEW// B.O TA AQUI
+
+            class ReviewAsyncTask extends AsyncTask<Movie, Movie, Movie> {
+
+                @Override
+                protected void onPostExecute(Movie movie) {
+                    MovieDetailActivity.this.movie = movie;
+                    mRecyclerViewReview.setAdapter(new ReviewAdapter(movie));
+                    super.onPostExecute(movie);
+                }
+
+                @Override
+                protected Movie doInBackground(Movie... movies) {
+                    HttpURLConnection urlConnection;
+
+                    movie = movies[0];
+
+                    try {
+                        URL url;
+                        String urls = "http://api.themoviedb.org/3/movie/" + movie.getId() + "/reviews";
+
+                        url = new URL(Uri.parse(urls)
+                                .buildUpon()
+                                .appendQueryParameter("api_key", appKey)
+                                .build().toString());
+
+                        urlConnection = (HttpURLConnection) url.openConnection();
+
+                        try {
+                            StringBuilder content = new StringBuilder();
+                            try (InputStream in = urlConnection.getInputStream();
+                                 Scanner data = new Scanner(in)) {
+                                data.useDelimiter("\n");
+                                while (data.hasNext()) {
+                                    content.append(data.next());
+                                }
+
+                                JSONObject json = new JSONObject(content.toString());
+                                JSONArray results = json.getJSONArray("results");
+
+
+                                for (int i = 0; i < results.length(); i++) {
+                                    json = results.getJSONObject(i);
+
+                                    movie.addReview(json.getString("author"),
+                                            json.getString("content"));
+                                }
+                            }
+                        } finally {
+                            urlConnection.disconnect();
+                        }
+                    } catch (Exception cause) {
+                        Log.e("MV", cause.getMessage(), cause);
+                    }
+
+                    return movie;
+                }
+
+            }
+
+            class ReviewViewHolder extends RecyclerView.ViewHolder {
+                ReviewViewHolder(View itemView) {
+                    super(itemView);
+                }
+            }
+
+            class ReviewAdapter extends RecyclerView.Adapter {
+
+                private Movie movie;
+
+                ReviewAdapter(Movie movie) {
+                    this.movie = movie;
+                }
+
+                @Override
+                public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                    View view = LayoutInflater.from(MovieDetailActivity.this)
+                            .inflate(R.layout.review_item_view, parent, false);
+                    return new ReviewViewHolder(view);
+                }
+
+                @Override
+                public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                    TextView mTextView1 = holder.itemView.findViewById(R.id.tv_name_review);
+                    TextView mTextView2 = holder.itemView.findViewById(R.id.tv_comment_review);
+
+                    mTextView1.setText(movie.getReviewName(position));
+                    mTextView2.setText(movie.getReviewComment(position));
+
+                }
+
+                @Override
+                public int getItemCount() {
+                    return movie.getReviewsSize();
+                }
+
+            }
         }
     }
-
-    class ReviewAdapter extends RecyclerView.Adapter {
-
-        private Movie movie;
-
-        ReviewAdapter(Movie movie) {
-            this.movie = movie;
-        }
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(MovieDetailActivity.this)
-                    .inflate(R.layout.review_item_view, parent, false);
-            return new ReviewViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            TextView mTextView1 = holder.itemView.findViewById(R.id.tv_name_review);
-            TextView mTextView2 = holder.itemView.findViewById(R.id.tv_comment_review);
-
-            mTextView1.setText(movie.getReviewName(position));
-            mTextView2.setText(movie.getReviewComment(position));
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return movie.getReviewsSize();
-        }
-
-}
+*/
 }
